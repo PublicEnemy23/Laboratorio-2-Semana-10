a59324dab77fc105b6f57069cc275524777ccc7b
PublicEnemy23 <142042673+PublicEnemy23@users.noreply.github.com>
2023-10-05 18:07:39 -0600

    Initial commit

---
+++ Lab2 05-10-23 (Segundo ejercicio)/Lab2 05-10-23 (Segundo ejercicio)/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim totalCompra As Double = 0
+
+        Console.WriteLine("Ingrese los montos de las compras (ingrese un monto negativo para finalizar):")
+
+        ' Bucle para ingresar los montos de las compras
+        While True
+            Console.Write("Monto de la compra: $")
+            Dim montoCompra As Double = Double.Parse(Console.ReadLine())
+
+            ' Verificar si se ingresó un monto negativo para finalizar
+            If montoCompra < 0 Then
+                Exit While
+            End If
+
+            ' Acumular el monto de la compra al total
+            totalCompra += montoCompra
+
+            ' Aplicar descuentos según las condiciones
+            If totalCompra > 999 Then
+                totalCompra *= 0.9 ' Aplicar un 10% de descuento
+            ElseIf totalCompra > 499 Then
+                totalCompra *= 0.95 ' Aplicar un 5% de descuento
+            End If
+        End While
+
+        ' Mostrar el total de la compra después de aplicar descuentos
+        Console.WriteLine("Total de la compra después de descuentos: $" & totalCompra.ToString("0.00"))
+
+        ' Esperar a que el usuario presione una tecla antes de salir
+        Console.WriteLine("Presione cualquier tecla para salir...")
+        Console.ReadKey()
+        Console.WriteLine("Parcial 2 de Programacion 2 Ing. Jonathan Carballo")
+        Console.WriteLine("Desarrollado por Mario Enrique Menjivar Abarca.")
+        Console.WriteLine("Fecha 05/10/2023")
+    End Sub
+
+End Module
---
+++ Lab2 05-10-23/Lab2 05-10-23/Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Declaración de variables
+        Dim costo As Double
+        Dim dineroEntregado As Double
+        Dim cambio As Double
+
+        ' Solicitar el costo del artículo
+        Console.Write("Ingrese el costo del artículo: ")
+        costo = Double.Parse(Console.ReadLine())
+
+        ' Solicitar la cantidad de dinero entregado por el cliente
+        Console.Write("Ingrese la cantidad de dinero entregado por el cliente: ")
+        dineroEntregado = Double.Parse(Console.ReadLine())
+
+        ' Verificar el pago y calcular el cambio o la cantidad faltante
+        If dineroEntregado > costo Then
+            cambio = dineroEntregado - costo
+            Console.WriteLine("Cambio a entregar: $" & cambio.ToString("0.00"))
+        ElseIf dineroEntregado = costo Then
+            Console.WriteLine("Entrega del producto.")
+        Else
+            Console.WriteLine("El pago es insuficiente. Falta pagar: $" & (costo - dineroEntregado).ToString("0.00"))
+            Console.WriteLine("Lo sentimos, no podemos entregar el producto en este momento.")
+
+        End If
+        Console.WriteLine("Vuelva pronto por el producto que desea.")
+        ' Esperar a que el usuario presione una tecla antes de salir
+        Console.WriteLine("Presione cualquier tecla para salir...")
+        Console.ReadKey()
+        Console.WriteLine("Laboratorio 2 de Programacion 2 Ing. Jonathan Carballo")
+        Console.WriteLine("Desarrollado por Mario Enrique Menjivar Abarca.")
+        Console.WriteLine("Fecha 05/10/2023")
+    End Sub
+
+End Module
